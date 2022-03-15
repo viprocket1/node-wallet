40f9155d58a0f042021c6baabec0af49b66c00fc
DESKTOP-P2VSRRP\tanmay <mrcooldinesh1@gmail.com>
2022-03-15 16:15:49 +0530

    methods

--- index.js
+++ index.js
@@ -12,22 +12,33 @@ import { clusterApiUrl, Connection, PublicKey } from "@solana/web3.js";
 const connection = new Connection(clusterApiUrl("devnet"));
 const mnemonic =
   "gold shy refuse quit gadget suffer slim approve air nephew orient gather drastic inherit layer curve idea soul dad dizzy boring tattoo raccoon scale";
+const destination = new PublicKey(
+  "FfLczWEt8wYoubArWX6iTKJ85inoKinvovzJ4CqRRuzX"
+);
+const tokenAddress = new PublicKey(
+  "9QPeHFjjyw4jy9GDzs3RvWxWJuwpKBj6RzQpEnwBuAnR"
+);
 
 (async () => {
-  //const seed = await mnemonicToSeed(mnemonic);
-  //storeMnemonicAndSeed(mnemonic, seed, "password", "bip44Change");
-  const m_s = await loadMnemonicAndSeed("password", "true");
-  console.log(m_s);
+  // create new wallet
+  const mnemonicAndSeed = await generateMnemonicAndSeed();
+
+  //restore old wallet
+  const seed = await mnemonicToSeed(mnemonic);
   const account = await getAccountFromSeed(m_s.seed, 0, m_s.derivationPath, 0);
-  console.log(account.publicKey.toBase58());
+
+  //passcode wallet
+  storeMnemonicAndSeed(mnemonic, seed, "password", "bip44Change");
+
+  //load a saved wallet from localStorage
+  const m_s = await loadMnemonicAndSeed("password", "true");
+
+  //get a wallet object
   const wallet = new Wallet(connection, "local", { account });
-  const destination = new PublicKey(
-    "FfLczWEt8wYoubArWX6iTKJ85inoKinvovzJ4CqRRuzX"
-  );
-  const tokenAddress = new PublicKey(
-    "9QPeHFjjyw4jy9GDzs3RvWxWJuwpKBj6RzQpEnwBuAnR"
-  );
-  console.log(wallet);
-  //wallet.createTokenAccount(tokenAddress);
+
+  //transfer solana
   wallet.transferSol(destination, 50000);
+
+  //transfer token
+  wallet.transferToken(tokenAddress, destination, 50000);
 })();
